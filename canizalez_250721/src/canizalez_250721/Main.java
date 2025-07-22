8015f46ba8cb0930aca7233dad89c8d2c0c38989
Alessandra Gabriela Canizalez Martinez <Ia.gabycani@ufg.edu.sv>
2025-07-21 18:27:38 -0600

    practica del 21 de julio del 2025

---
+++ canizalez_250721/src/canizalez_250721/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package canizalez_250721;
+
+/**
+ *
+ * @author UFG
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("50 grados centigrados a farhrenteit " + celsius_a_fahrenteit()+ "f"); 
+   System.out.println("50 grados centigrados a farhrenteit " + celsius_a_kelvin()+ "k"); 
+   System.out.println ("50 grados centigrados a farhrenteit " + celsius_a_rankie()+ "r"); 
+   System.out.println("50 grados centigrados a farhenteit ");
+    }
+    public static double celsius_a_fahrenteit(){
+        double grados_centigrados = 50.0;
+        return ((grados_centigrados * 9) / 5)+32;
+    
+    } 
+    public static double celsius_a_kelvin(){
+        double grados_centigrados  = 50.0;
+        return grados_centigrados + 273;
+        
+    }
+    public static double celsius_a_rankie(){
+        double grados_centigrados = 50.0;
+        return ((grados_centigrados * 9)) / 5 +491.67;
+    }
+    public static double fahrenteit_a_celsius(){
+        double grados_fahrenteit= 30.0;
+        return (( grados_fahrenteit - 32)) *5 / 9;
+    }
+    public static double fahrenteit_a_kelvin(){
+        double grados_fahrenteit = 30.0;
+        return ((grados_fahrenteit - 32 ) *5 / 9) + 273.15;
+    }
+}
